da84c21a6b358c3544cac5e09bf542b42b072ded
Philipp Konrad <philipp.konrad@qbeyond>
2023-10-01 20:14:50 +0200

    - logic update

--- controller/main.py
+++ controller/main.py
@@ -81,7 +81,9 @@ def read_door():
     response = requests.get(f'{base_url}/get/door/position')
     if response.status_code == 200:
         result = response.json().get('value')
+        result= True
         return result
+
     return False
 
 # Main controller logic
@@ -91,13 +93,15 @@ def main():
         try:
             if read_door() == False:
                 # door is close
-                if not is_time_to_close():
+
+                if not is_time_to_close() and is_bright_enough() == True:
+
                     if is_time_to_open() or is_bright_enough():
                         open_door()
 
             if read_door() == True:
                 # door is open
-                if is_time_to_close() or not is_bright_enough():
+                if is_time_to_close() or is_bright_enough() == False:
                     close_door()
 
             # Sleep for a specified interval (e.g., 5 minutes)
